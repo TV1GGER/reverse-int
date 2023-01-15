52a2f4f40ce1db8b675b3ccdb320f7d4b935c816
Dzianis Perahudau <tvigger@yandex.ru>
2023-01-15 17:20:16 +0300

    feat: add Reverse Int code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let m = Math.abs(n);
+    let string = m.toString();
+    let result = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+    result = result + string[i];
+    }
+    return Number(result);
+};
